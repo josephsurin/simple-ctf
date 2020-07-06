5e85f3a7048076d4593c2e9508b908add124d746
fumetsuu <joseph.surin@gmail.com>
2020-07-07 00:06:07 +1000

    test challs

--- server/routes/api.js
+++ server/routes/api.js
@@ -29,10 +29,16 @@ router.post('/login', passport.authenticate('local'), (req, res) => {
     res.json({ msg: 'Login Successful', token })
 })
 
-
+const testchalls = [
+    { id: 0, sortWeight: -1, title: 'test chall 1', category: 'misc', description: 'helo this is the first challenge in the series of challenge tests this should also spuport markdown, take a look at next couple of chall descriptions to see !!', files: [], points: 1, solves: 4 },
+    { id: 1, sortWeight: 1, title: 'test chall 2', category: 'cryptography', description: 
+        `challenge 2!!! checkout the *markdown* **in this one** ooo \`fancy!\` even \`\`\`python
+code blocks are supported with "syntax highlighting" 3+4 in for\`\`\``, files: [], points: 99, solves: 0 },
+    { id: 2, sortWeight: 4, title: 'test chllenge 3 this one has a pretty long title ohpefuly thats ok', category: 'binary exploitation', description: 'yes gogogo. source code with this chall! take a look at the FIlesssss `nc helo.com 123`', files: ['/files/1234.c'], points: 1337, solves: 1  }
+]
 router.get('/challenges', ensureAuthenticated, (req, res) => {
     console.log(req.user)
-    res.json({ msg: 'got challenges' })
+    res.json({ msg: 'got challenges', challenges: testchalls })
 })
 
 module.exports = router
