5ccc70915fa272a94dda76da49de1a980459b9b9
fumetsuu <joseph.surin@gmail.com>
2020-07-08 21:28:25 +1000

    add time to submission schema

--- server/models/submission.js
+++ server/models/submission.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const submissionsSchema = new mongoose.Schema({
     user: { type: String, required: true },
     chall: { type: String, required: true },
-    submission: { type: String, required: true }
+    submission: { type: String, required: true },
+    time: { type: Date, default: Date.now }
 })
 
 module.exports = mongoose.model('Submission', submissionsSchema)
