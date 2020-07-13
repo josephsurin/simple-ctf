8374ab90541aeee2f9b5fa7940158a55ab1be9a2
fumetsuu <joseph.surin@gmail.com>
2020-07-13 12:15:40 +1000

    add/remove team member in edit profile page

--- client/src/routes/editprofile/index.js
+++ client/src/routes/editprofile/index.js
@@ -4,7 +4,12 @@ import style from './style.sass'
 import { apiRequest } from '../../util'
 
 class EditProfile extends Component {
-    state = { username: '', currentEmail: '', email: '', newpassword: '', oldpassword: '', msg: '', success: null }
+    state = { username: '', memberEmail: '', currentEmail: '', email: '', newpassword: '', oldpassword: '', members: [], msg: '', success: null }
+
+    onMemberEmailChange = e => {
+        let { value } = e.target
+        this.setState({ memberEmail: value })
+    }
 
     onEmailChange = e => {
         let { value } = e.target
@@ -20,6 +25,18 @@ class EditProfile extends Component {
         let { value } = e.target
         this.setState({ oldpassword: value })
     }
+    
+    onAddMemberSubmit = e => {
+        let { memberEmail } = this.state
+        var data = { memberEmail }
+        apiRequest('/addmember', { method: 'POST', body: JSON.stringify(data) })
+            .then(r => {
+                if(r.err) return this.setState({ msg: r.err, success: false })
+                this.loadData()
+                return this.setState({ msg: r.msg, success: true })
+            }).catch(err => console.log('error occurred: ', err))
+        e.preventDefault()
+    }
 
     onChangeEmailSubmit = e => {
         let { email } = this.state
@@ -29,7 +46,7 @@ class EditProfile extends Component {
                 if(r.err) return this.setState({ msg: r.err, success: false })
                 this.loadData()
                 return this.setState({ msg: r.msg, success: r.msg != 'rate limited' })
-            }).catch(err => console.log('error occured: ', err))
+            }).catch(err => console.log('error occurred: ', err))
         e.preventDefault()
     }
 
@@ -41,8 +58,18 @@ class EditProfile extends Component {
                 if(r.err) return this.setState({ msg: r.err, success: false })
                 this.loadData()
                 return this.setState({ msg: r.msg, success: r.msg != 'rate limited' })
-            }).catch(err => console.log('error occured: ', err))
+            }).catch(err => console.log('error occurred: ', err))
         e.preventDefault()
+    }
+
+    removeMember = memberEmail => {
+        var data = { memberEmail }
+        apiRequest('/removemember', { method: 'POST', body: JSON.stringify(data) })
+            .then(r => {
+                if(r.err) return this.setState({ msg: r.err, success: false })
+                this.loadData()
+                return this.setState({ msg: r.msg, success: true })
+            }).catch(err => console.log('error occurred: ', err))
     }
 
     componentDidMount = () => {
@@ -51,18 +78,24 @@ class EditProfile extends Component {
 
     loadData = () => {
         apiRequest('/getdetails')
-            .then(r => this.setState({ username: r.username, currentEmail: r.email }))
-            .catch(err => console.log('error occured: ', err))
+            .then(r => this.setState({ username: r.username, currentEmail: r.email, members: r.members }))
+            .catch(err => console.log('error occurred: ', err))
     }
 
-    render(_, { username, currentEmail, email, oldpassword, newpassword, msg, success }) {
+    render(_, { username, memberEmail, currentEmail, email, oldpassword, newpassword, members, msg, success }) {
         return (
             <div class={style.editprofile}>
                 <h1>Edit Profile</h1>
                 <div class={style.current_details}>
                     username: <b>{username}</b> <br/>
-                    email: <b>{currentEmail}</b>
+                    email: <b>{currentEmail}</b> <br/>
+                    other team members:
+                        <ul>{members.length == 0 ? <b>none yet!</b> : members.map(member => <li><b>{member} <a onClick={this.removeMember.bind(this, member)}>[X]</a></b></li>)}</ul>
                 </div>
+                <form class={style.email_form} onSubmit={this.onAddMemberSubmit}>
+                    <input type="email" placeholder="add member email" value={memberEmail} required="required" onInput={this.onMemberEmailChange} />
+                    <button type="submit">Add Member</button>
+                </form>
                 <form class={style.email_form} onSubmit={this.onChangeEmailSubmit}>
                     <input type="email" placeholder="new email" value={email} required="required" onInput={this.onEmailChange} />
                     <button type="submit">Update Email</button>
