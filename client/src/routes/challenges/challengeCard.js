ccc8ac3e386c558cd55c241aea7099883fefa3f2
fumetsuu <joseph.surin@gmail.com>
2020-07-20 12:09:03 +1000

    client side feedback for number of attempts, disable forms when submitting and when no more attempts

--- client/src/routes/challenges/challengeCard.js
+++ client/src/routes/challenges/challengeCard.js
@@ -9,7 +9,7 @@ import Modal from '../../components/modal'
 const md = markdownit({ html: true })
 
 class ChallengeCard extends Component {
-    state = { isOpen: false, submitting: false, submission: '', status: null }
+    state = { isOpen: false, submitting: false, submission: '', numAttemptsLeft: null, status: null }
 
     toggleOpen = () => {
         this.setState({ isOpen: !this.state.isOpen })
@@ -33,39 +33,45 @@ class ChallengeCard extends Component {
             apiRequest('/submit', { method: 'POST', body: JSON.stringify(data) })
                 .then(r => {
                     if(r.msg == 'correct') this.props.onSolve()
-                    this.setState({ status: r.msg, submitting: false })
+                    var numAttemptsLeft = r.numAttemptsLeft == null ? this.state.numAttemptsLeft : r.numAttemptsLeft
+                    this.setState({ status: r.msg, numAttemptsLeft, submitting: false })
                 })
         })
     }
 
-    renderDetails = (id, description, files, submitting, submission, status) => (
-        <div class={style.details}>
-            <div class={style.description} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(md.render(description)) }}>
-            </div>
-            {files.length === 0 ? null :
-            <div class={style.files_container}>
-                Files:
-                {files.map(relPath => {
-                    var filename = path.basename(relPath)
-                    var filePath = genStaticFilePath(path.join(id, relPath))
-                    return <a class={style.file_link} href={filePath} target="_blank" native download="download">{filename}</a>
-                })}
-            </div>}
-            <div class={style.submission_container}>
-                <form class={style.submission_form} onSubmit={this.submitFlag}>
-                    <input class={submitting ? style.disabled : ''} type="text" placeholder="flag" value={submission} onInput={this.onSubmissionChange} />
-                    <button class={submitting ? style.disabled : ''} type="submit">Submit</button>
-                </form>
-            </div>
-            {status === 'incorrect' ? <div class={style.submission_incorrect}>Incorrect Flag</div> :
-             status === 'correct' ? <div class={style.submission_correct}>Correct!</div> :
-             status === 'already solved' ? <div class={style.submission_already_solved}>Already Solved</div> :
-             status === 'rate limited' ? <div class={style.submission_incorrect}>Submitting too fast. Slow down!</div> : null}
-        </div>
-    )
+    renderDetails = (id, description, files, submitting, submission, numAttemptsLeft, status) => {
+        var disabled = submitting || numAttemptsLeft <= 0
+        return (
+            <div class={style.details}>
+                <div class={style.description} dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(md.render(description)) }}>
+                </div>
+                {files.length === 0 ? null :
+                <div class={style.files_container}>
+                    Files:
+                    {files.map(relPath => {
+                        var filename = path.basename(relPath)
+                        var filePath = genStaticFilePath(path.join(id, relPath))
+                        return <a class={style.file_link} href={filePath} target="_blank" native download="download">{filename}</a>
+                    })}
+                </div>}
+                <div class={style.submission_container}>
+                    <form class={style.submission_form} onSubmit={this.submitFlag}>
+                        <input disabled={disabled} class={disabled ? style.disabled : ''} type="text" placeholder="flag" value={submission} onInput={this.onSubmissionChange} />
+                        <button disabled={disabled} class={disabled ? style.disabled : ''} type="submit">Submit</button>
+                    </form>
+                    {numAttemptsLeft != null ? <div class={style.attempts_warning}>You have {numAttemptsLeft <= 0 ? 0 : numAttemptsLeft} attempts left for this challenge.</div> : null}
+                </div>
+                {status === 'incorrect' ? <div class={style.submission_incorrect}>Incorrect Flag</div> :
+                 status === 'correct' ? <div class={style.submission_correct}>Correct!</div> :
+                 status === 'already solved' ? <div class={style.submission_already_solved}>Already Solved</div> :
+                 status === 'rate limited' ? <div class={style.submission_incorrect}>Submitting too fast. Slow down!</div> :
+                 status === 'max attempts' ? <div class={style.submission_incorrect}>Max Attempts Reached</div> : null}
+            </div>)
+    }
 
-    render({ data, cardStyle }, { isOpen, submitting, submission, status }) {
-        let { id, title, category, points, description, numSolves, files, solved } = data
+    render({ data, cardStyle }, { isOpen, submitting, submission, numAttemptsLeft, status }) {
+        let { id, title, category, points, description, numSolves, files, solved, attempts, maxAttempts } = data
+        numAttemptsLeft = numAttemptsLeft == null ? (maxAttempts - attempts) : numAttemptsLeft
         return (
             <div class={cardStyle == 'list' ? style.list_challenge_card : cardStyle == 'tile' ? style.tile_challenge_card : ''}>
                 {cardStyle == 'list' ?
@@ -85,14 +91,14 @@ class ChallengeCard extends Component {
                 }
                 {!isOpen ? null :
                 cardStyle == 'list' ?
-                <Fragment>{this.renderDetails(id, description, files, submitting, submission, status)}</Fragment>
+                <Fragment>{this.renderDetails(id, description, files, submitting, submission, numAttemptsLeft, status)}</Fragment>
                     :
                 cardStyle == 'tile' ?
                 <Modal open={isOpen} onDismiss={this.closeModal}>
                     <div class={style.tile_modal_inner}>
                         <div class={style.title}>{title}</div>
                         <div class={style.points_and_solves}>{points} {points === 1 ? 'point' : 'points'} / {numSolves} {numSolves === 1 ? 'solve' : 'solves'}</div>
-                        {this.renderDetails(id, description, files, submitting, submission, status)}
+                        {this.renderDetails(id, description, files, submitting, submission, numAttemptsLeft, status)}
                     </div>
                 </Modal>
                     :
